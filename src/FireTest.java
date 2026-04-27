4a4769ed477f642920f6594f6eb884fc8a99e3fd
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-04-27 15:56:02 -0700

    3rd test

--- src/FireTest.java
+++ src/FireTest.java
@@ -40,4 +40,23 @@ public class FireTest {
 
         assertEquals(expected, actual);
     }
+
+        @Test
+    public void testTimeToBurnDExample() {
+        char[][] forest = {
+          {'t','.','.'},
+          {'.','t','.'},
+          {'.','.','t'},
+          {'.','t','.'},
+          {'t','.','.'},
+        };
+
+        int matchR = 1;
+        int matchC = 1;
+
+        int expected = 0;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+    }
 }
